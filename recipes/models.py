d448ac1a60689f59993cefe4e87574a7d686691f
Lola Crispin <swetlanda@gmail.com>
2024-07-29 19:57:35 +0000

    Added a new model Recipe

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
 
 # Create your models here.
+class Recipe(models.Model):
+    user = models.ForeignKey(
+            User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    serving = models.CharField(max_length=200, unique=True)
+    
+
+
